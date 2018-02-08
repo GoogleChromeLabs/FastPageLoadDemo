0b0f924a240c3e7220a4e7d0bebc889368efc405
Dave Gash <dgash@google.com>
2018-02-08 10:53:03 -0800

    Add license headers

--- resources1/sorttable.js
+++ resources1/sorttable.js
@@ -1,3 +1,18 @@
+/*
+Copyright 2018 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 var europeandate=!1;var alternate_row_colors=!0;addEvent(window,"load",sortables_init);var SORT_COLUMN_INDEX;var thead=!1;function sortables_init(){if(!document.getElementsByTagName)return;tbls=document.getElementsByTagName("table");for(ti=0;ti<tbls.length;ti++){thisTbl=tbls[ti];if(((' '+thisTbl.className+' ').indexOf("sortable")!=-1)&&(thisTbl.id)){ts_makeSortable(thisTbl)}}}
 function ts_makeSortable(t){if(t.rows&&t.rows.length>0){if(t.tHead&&t.tHead.rows.length>0){var firstRow=t.tHead.rows[t.tHead.rows.length-1];thead=!0}else{var firstRow=t.rows[0]}}
 if(!firstRow)return;for(var i=0;i<firstRow.cells.length;i++){var cell=firstRow.cells[i];var txt=ts_getInnerText(cell);if(cell.className!="unsortable"&&cell.className.indexOf("unsortable")==-1){cell.innerHTML='<a href="#" class="reg title" onclick="ts_resortTable(this, '+i+');return false;">'+txt+'</a>'}}
--- resources2/sorttable.js
+++ resources2/sorttable.js
@@ -1,11 +1,17 @@
 /*
-Table sorting script  by Joost de Valk, http://www.joostdevalk.nl/code/sortable-table/
-Based on a script from http://www.kryogenix.org/code/browser/sorttable/
-Distributed under the MIT license: http://www.kryogenix.org/code/browser/licence.html
+Copyright 2018 Google LLC
 
-Copyright (c) 1997-2007 Stuart Langridge, Joost de Valk.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-Version 1.5.7
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
 /* You can change these values */
